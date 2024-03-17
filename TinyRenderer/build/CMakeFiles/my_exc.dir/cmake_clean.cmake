c7ef9121cdc1e5473ecf24352b068d93bf2d0423
floran <floran.defossez@gmail.com>
2024-03-17 16:20:55 +0100

    first triangle

---
+++ TinyRenderer/build/CMakeFiles/my_exc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_exc.dir/src/glad.c.o"
+  "CMakeFiles/my_exc.dir/src/glad.c.o.d"
+  "CMakeFiles/my_exc.dir/src/main.c.o"
+  "CMakeFiles/my_exc.dir/src/main.c.o.d"
+  "my_exc"
+  "my_exc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_exc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
